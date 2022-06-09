d32dce78b50ba1c94ad75a39705d21fcb1415f83
Bruno Giovagnoli <brunogiovagnoli@gmail.com>
2022-06-09 19:10:01 -0300

    added access to mongodb catalog db + dockerfile for Catalog.API + docker compose

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var connectionString = configuration["DatabaseSettings:ConnectionString"];
+            var databaseName = configuration["DatabaseSettings:DatabaseName"];
+            var collectionName = configuration["DatabaseSettings:CollectionName"];
+
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase(databaseName);
+
+            Products = database.GetCollection<Product>(collectionName);
+            CatalogContextSeed.SeedData(Products); // TODO: move this to Program.cs and make it configurable via settings
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
