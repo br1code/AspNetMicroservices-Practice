fb77da7e9a723fe1313d1829fc0189a4d50ac0e4
Bruno Giovagnoli <brunogiovagnoli@gmail.com>
2022-06-14 14:54:30 -0300

    create Basket.API (code and docker-compose) + add portainer

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,50 @@
+using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Basket.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+
+        public BasketController(IBasketRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet("{username}", Name = "GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string username)
+        {
+            var basket = await _repository.GetBasket(username);
+            return Ok(basket ?? new ShoppingCart(username));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            var updated = await _repository.UpdateBasket(basket);
+
+            if (updated == null)
+            {
+                return BadRequest("Unable to get or update basket.");
+            }
+
+            return Ok(updated);
+        }
+
+        [HttpDelete("{username}", Name = "DeleteBasket")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteBasket(string username)
+        {
+            await _repository.DeleteBasket(username);
+            return Ok();
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,55 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _cache;
+
+        public BasketRepository(IDistributedCache cache)
+        {
+            _cache = cache ?? throw new ArgumentNullException(nameof(cache));
+        }
+
+        public async Task DeleteBasket(string username)
+        {
+            if (string.IsNullOrEmpty(username))
+            {
+                throw new ArgumentNullException(nameof(username));
+            }
+
+            await _cache.RemoveAsync(username);
+        }
+
+        public async Task<ShoppingCart?> GetBasket(string username)
+        {
+            if (string.IsNullOrEmpty(username))
+            {
+                throw new ArgumentNullException(nameof(username));
+            }
+
+            var basket = await _cache.GetStringAsync(username);
+
+            if (string.IsNullOrEmpty(basket))
+            {
+                return null;
+            }
+
+            return JsonSerializer.Deserialize<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart?> UpdateBasket(ShoppingCart basket)
+        {
+            if (basket == null)
+            {
+                throw new ArgumentNullException(nameof(basket));
+            }
+
+            var serializedBasket = JsonSerializer.Serialize(basket);
+            await _cache.SetStringAsync(basket.Username, serializedBasket);
+            return await GetBasket(basket.Username);
+        }
+    }
+}
